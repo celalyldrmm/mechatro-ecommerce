4c85cdb1820bcc302e656318556ab9344e5cee23
Hamit Can Sayılgan <hamitcansayilgan@gmail.com>
2024-06-08 21:06:46 +0300

    Feat: add category, cart, cart process, orders, confirm pages to HomeController and implement methods for retrieving products.
    - Added Category, Cart, Cart Process, Orders, and Confirm pages to the HomeController for navigation.
    - Implemented methods in the HomeController for retrieving products data.
    - Created corresponding views for the added pages in the Views/Home directory.
    - Added stored procedure SP_Search to the SP_Search model for product search functionality.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using mechatro_ecommerce.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace mechatro_ecommerce.Controllers
@@ -159,9 +160,204 @@ namespace mechatro_ecommerce.Controllers
             }
         }
 
+        public IActionResult CategoryPage(int id)
+        {
+            List<Product> products = productRepository.ProductSelectWithCategoryID(id);
+            return View(products);
+        }
+
+        public IActionResult Cart()
+        {
+            List<OrderRepository> sepet;
+
+            if (HttpContext.Request.Query["scid"].ToString() != "")
+
+            {
+
+                string? scid = HttpContext.Request.Query["scid"];
+                orderRepository.MyCart = Request.Cookies["sepetim"];
+                orderRepository.DeleteFromMyCart(scid);
+                var cookieOptions = new CookieOptions();
+                Response.Cookies.Append("sepetim", orderRepository.MyCart, cookieOptions);
+                cookieOptions.Expires = DateTime.Now.AddDays(1);
+                TempData["Message"] = "Ürün Sepetinizden Silindi";
+                sepet = orderRepository.SelectMyCart();
+                ViewBag.Sepetim = sepet;
+                ViewBag.sepet_tablo_detay = sepet;
+            }
+            else
+            {
+                var cookie = Request.Cookies["sepetim"];
+                if (cookie == null)
+                {
+                    orderRepository.MyCart = "";
+                    sepet = orderRepository.SelectMyCart();
+                    ViewBag.Sepetim = sepet;
+                    ViewBag.sepet_tablo_detay = sepet;
+                }
+                else
+                {
+                    var cookieOptions = new CookieOptions();
+                    orderRepository.MyCart = Request.Cookies["sepetim"];
+                    sepet = orderRepository.SelectMyCart();
+                    ViewBag.Sepetim = sepet;
+                    ViewBag.sepet_tablo_detay = sepet;
+                }
+
+            }
+            mpm.FeaturedProducts = productRepository.ProductSelect("Featured", mainpageCount, "Featured", 0);
+            return View(mpm);
+        }
+
+        public IActionResult CartProcess(int id)
+        {
+
+            orderRepository.ProductID = id;
+            orderRepository.Quantity = 1;
+
+            var cookieOptions = new CookieOptions();
+
+            var cookie = Request.Cookies["sepetim"];
+
+            if (cookie == null)
+            {
+
+                cookieOptions = new CookieOptions();
+                cookieOptions.Expires = DateTime.Now.AddDays(1);
+                cookieOptions.Path = "/";
+                orderRepository.MyCart = "";
+                orderRepository.AddToMyCart(id.ToString());
+                Response.Cookies.Append("sepetim", orderRepository.MyCart, cookieOptions);
+                TempData["Message"] = "Ürün Sepetinize Eklendi";
+            }
+            else
+            {
+                orderRepository.MyCart = cookie;
+                if (orderRepository.AddToMyCart(id.ToString()) == false)
+                {
+                    HttpContext.Response.Cookies.Append("sepetim", orderRepository.MyCart, cookieOptions);
+                    cookieOptions.Expires = DateTime.Now.AddDays(1);
+                    TempData["Message"] = "Ürün Sepetinize Eklendi";
+                }
+                else
+                {
+
+                    TempData["Message"] = "Bu Ürün Zaten Sepetinizde Var";
+
+                }
+            }
+
+            string url = Request.Headers["Referer"].ToString();
+            return Redirect(url);
+        }
+        public IActionResult Order()
+        {
+            if (HttpContext.Session.GetString("Email") != null)
+            {
+                User? usr = userRepository.SelectMemberInfo(HttpContext.Session.GetString("Email"));
+                return View(usr);
+            }
+            else
+            {
+
+                return RedirectToAction("Login");
+            }
+        }
+        public IActionResult MyOrders()
+        {
+            if (HttpContext.Session.GetString("Email") != null)
+            {
+                var mainpagemodel = new MainPageModel();
+                mainpagemodel.MyOrders = orderRepository.SelectMyOrders(HttpContext.Session.GetString("Email").ToString());
+                mainpagemodel.RealetedProducts = productRepository.ProductSelect("RealetedProducts", mainpageCount, "RealetedProducts", 0);
+
+                return View(mainpagemodel);
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult Order(IFormCollection frm)
+        {
+            string txt_individual = Request.Form["txt_individual"];
+            string txt_corporate = Request.Form["txt_corporate"];
+
+            if (txt_individual != null)
+            {
+                //bireysel fatura
+                //digital planet
+                // WebServiceController.tckimlik_vergi_no = txt_individual;
+                orderRepository.tckimlik_vergi_no = txt_individual;
+                orderRepository.EfaturaCreate();
+            }
+            else
+            {
+                //kurumsal fatura
+                //  WebServiceController.tckimlik_vergi_no = txt_corporate;
+                orderRepository.tckimlik_vergi_no = txt_corporate;
+                orderRepository.EfaturaCreate();
+            }
+
+            string kredikartno = Request.Form["kredikartno"];//1.yol = formun içindeki textboxalanýnn deðerini 
+            string kredikartay = frm["kredikartay"];//2.yol=formun içindeki textboxalanýnn deðerini 
+            string kredikartyil = frm["kredikartyil"];
+            string kredikartcvs = frm["kredikartcvs"];
+
+            return RedirectToAction("backref");
 
 
 
+
+        }
+        public IActionResult ConfirmPage()
+        {
+            ViewBag.OrderGroupGUID = OrderGroupGUID;
+            mpm.RealetedProducts = productRepository.ProductSelect("RealetedProducts", mainpageCount, "RealetedProducts", 0);
+            return View(mpm);
+        }
+        public IActionResult backref()
+
+        {
+            ConfirmOrder();
+            return RedirectToAction("ConfirmPage");
+        }
+
+        public static string OrderGroupGUID = "";
+
+        public IActionResult ConfirmOrder()
+
+        {
+            var cookieOptions = new CookieOptions();
+            var cookie = Request.Cookies["sepetim"];
+
+            if (cookie != null)
+
+            {
+
+                orderRepository.MyCart = cookie;
+                OrderGroupGUID = orderRepository.OrderCreate(HttpContext.Session.GetString("Email").ToString());
+                cookieOptions.Expires = DateTime.Now.AddDays(1);
+                Response.Cookies.Delete("sepetim");
+
+            }
+
+            return RedirectToAction("ConfirmPage");
+
+        }
+
+        public PartialViewResult gettingProducts(string id)
+        {
+            id = id.ToUpper(new System.Globalization.CultureInfo("tr-TR"));
+            List<SP_Search> ulist = ProductRepository.gettingSearchProducts(id);
+            string json = JsonConvert.SerializeObject(ulist);
+            var response = JsonConvert.DeserializeObject<List<Search>>(json);
+
+            return PartialView(response);
+        }
 
     }
 }
---
+++ Models/SP_Search.cs
@@ -0,0 +1,11 @@
+﻿namespace mechatro_project.Models
+{
+    public class SP_Search
+    {
+        public int ID { get; set; }
+        public int KATEGORI { get; set; }
+        public int URUN { get; set; }
+        public int MARKA { get; set; }
+        public string? ARAMAISMI { get; set; }
+    }
+}
