e919584d3a0f0537708d084c7385247199f86f46
Hamit Can Sayılgan <hamitcansayilgan@gmail.com>
2024-06-08 20:36:57 +0300

    Refactor: separate footer and menus components, and organize according to default layout components.

---
+++ Views/ViewComponents/Footer.cs
@@ -0,0 +1,12 @@
+﻿using mechatro_ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace mechatro_project.Views.ViewComponents
+{
+	public class Footer : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
---
+++ Views/ViewComponents/Menus.cs
@@ -0,0 +1,21 @@
+﻿using mechatro_ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace mechatro_project.Views.ViewComponents
+{
+    public class Menus : ViewComponent
+    {
+        MechatroContext context = new MechatroContext();
+
+        public IViewComponentResult Invoke()
+        {
+            List<Category> categories = context.Categories.ToList();
+            return View(categories);
+        }
+
+
+    }
+}
+
+
