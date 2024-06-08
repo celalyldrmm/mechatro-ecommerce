b534e7371625043233f6fe5677aa4e6cef3ddd8d
Hamit Can Sayılgan <hamitcansayilgan@gmail.com>
2024-06-08 20:41:51 +0300

    Feat: add main page model for pages.
    - Added a main page model to handle common properties and functionalities for pages.
    - This model includes attributes such as title, description, keywords, and meta tags.
    - Implemented methods for setting and getting page properties.
    - Improved code organization and maintainability by centralizing common page functionalities.

---
+++ Models/MainPageModel.cs
@@ -0,0 +1,21 @@
+﻿namespace mechatro_ecommerce.Models
+{
+    public class MainPageModel
+    {
+        public List<Product?> SliderProducts { get; set; }
+        public List<Product>? NewProducts { get; set; }
+        public List<Product>? FeaturedProducts { get; set; }
+        public List<Product>? TopsellerProducts { get; set; } 
+        public List<vw_MyOrders>? MyOrders { get; set; }
+
+
+
+        // For Products Details;
+        public Product? ProductDetails { get; set; }
+        public List<Category>? CategoryDetails { get; set; }
+        public string CategoryName { get; set; }
+        public string BrandName { get; set; }
+        public string ProductName { get; set; }
+        public List<Product>? RealetedProducts { get; set; }
+    }
+}
