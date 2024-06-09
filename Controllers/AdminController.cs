4adcbfed8cf1d1a9c941c7ce26c936a9a01f025d
Hamit Can Sayılgan <hamitcansayilgan@gmail.com>
2024-06-09 19:12:59 +0300

    Fix: list Products and Statuses in Admin Control Index.
    Refactor: clean up unnecessary spaces in Admin Controller and Index view.

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -2,27 +2,26 @@
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using mechatro_ecommerce.Models;
 using System.Linq;
+using mechatro_ecommerce.Models;
 
 namespace mechatro_ecommerce.Controllers
 {
 
     public class AdminController : Controller
     {
-		ICategoryRepository categoryRepository = new CategoryRepository();
-		UserRepository u = new UserRepository();
+        ICategoryRepository categoryRepository = new CategoryRepository();
+        UserRepository u = new UserRepository();
         CategoryRepository c = new CategoryRepository();
         SupplierRepository s = new SupplierRepository();
         ProductRepository p = new ProductRepository();
         StatusRepository st = new StatusRepository();
         SettingRepository setting = new SettingRepository();
 
-
         MechatroContext context = new MechatroContext();
 
 
-		[HttpGet]
+        [HttpGet]
 
         public IActionResult Login()
         {
@@ -37,7 +36,7 @@ namespace mechatro_ecommerce.Controllers
 
             if (user != null)
             {
-                
+
                 HttpContext.Session.SetString("LoginInfo", usernameOrEmail);
                 return RedirectToAction("Index");
             }
@@ -48,76 +47,73 @@ namespace mechatro_ecommerce.Controllers
             }
         }
 
-		public IActionResult Logout()
-		{
-			HttpContext.Session.Remove("LoginInfo");
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("LoginInfo");
             HttpContext.Session.Remove("Admin");
-			return RedirectToAction("Login");
-		}
-
-
-
-
-		public ActionResult Index()
-		{
-			MechatroContext context = new MechatroContext();
-			AdminViewModel avm = new AdminViewModel();
-
-			avm.Categories = context.Categories.ToList();
-			avm.Suppliers = context.Suppliers.ToList();
-            return View(avm);
+            return RedirectToAction("Login");
         }
 
-    
+        public ActionResult Index()
+        {
+            MechatroContext context = new MechatroContext();
+            AdminViewModel avm = new AdminViewModel();
 
+            avm.Categories = context.Categories.ToList();
+            avm.Suppliers = context.Suppliers.ToList();
+            avm.Products = context.Products.ToList();
+            avm.Statuses = context.Statuses.ToList();
+
+            return View(avm);
+        }
         public async Task<IActionResult> CategoryIndex()
-		{
-			List<Category> categories = await c.CategorySelect();
-			return View(categories);
-		}
+        {
+            List<Category> categories = await c.CategorySelect();
+            return View(categories);
+        }
 
 
-		[HttpGet]
-		public IActionResult CategoryCreate()
-		{
-			CategoryFill();
-			return View();
-		}
-		void CategoryFill()
-		{
-			List<Category> categories = c.CategorySelectMain();
-			ViewData["categoryList"] = categories.Select(c => new SelectListItem { Text = c.CategoryName, Value = c.CategoryID.ToString()});
-		}
+        [HttpGet]
+        public IActionResult CategoryCreate()
+        {
+            CategoryFill();
+            return View();
+        }
+        void CategoryFill()
+        {
+            List<Category> categories = c.CategorySelectMain();
+            ViewData["categoryList"] = categories.Select(c => new SelectListItem { Text = c.CategoryName, Value = c.CategoryID.ToString() });
+        }
 
-		[HttpPost]
-		public IActionResult CategoryCreate(Category category)
-		{
+        [HttpPost]
+        public IActionResult CategoryCreate(Category category)
+        {
 
-			bool answer = categoryRepository.CategoryInsert(category);
-			if (answer == true)
-			{
-				TempData["Message"] = category.CategoryName + " Eklendi";
-			}
-			else
-			{
-				TempData["Message"] = "HATA";
-			}
-			return RedirectToAction(nameof(CategoryCreate));
-		}
+            bool answer = categoryRepository.CategoryInsert(category);
+            if (answer == true)
+            {
+                TempData["Message"] = category.CategoryName + " Eklendi";
+            }
+            else
+            {
+                TempData["Message"] = "HATA";
+            }
+            return RedirectToAction(nameof(CategoryCreate));
+        }
 
-		[HttpGet]
-		public async Task<IActionResult> CategoryEdit(int? id)
-		{
-			CategoryFill();
-			if (id == null || context.Categories == null)
-			{
-				return NotFound();
-			}
-			var category = await c.CategoryDetails(id);
-			return View(category);
-		}
+        [HttpGet]
+        public async Task<IActionResult> CategoryEdit(int? id)
+        {
+            CategoryFill();
+            if (id == null || context.Categories == null)
+            {
+                return NotFound();
+            }
+            var category = await c.CategoryDetails(id);
+            return View(category);
+        }
 
-		[HttpPost]
+        [HttpPost]
         public IActionResult CategoryEdit(Category category)
         {
 
@@ -135,151 +131,151 @@ namespace mechatro_ecommerce.Controllers
         }
 
         public async Task<IActionResult> CategoryDetails(int? id)
-		{
-			var category = await context.Categories.FirstOrDefaultAsync(c => c.CategoryID == id);
-			ViewBag.categoryname = category?.CategoryName;
+        {
+            var category = await context.Categories.FirstOrDefaultAsync(c => c.CategoryID == id);
+            ViewBag.categoryname = category?.CategoryName;
 
-			return View(category);
-		}
+            return View(category);
+        }
 
-		public async Task<IActionResult> CategoryDelete(int? id)
-		{
-			if (id == null || context.Categories == null)
-			{
-				return NotFound();
-			}
-			var category = await context.Categories.FirstOrDefaultAsync(c => c.CategoryID == id);
+        public async Task<IActionResult> CategoryDelete(int? id)
+        {
+            if (id == null || context.Categories == null)
+            {
+                return NotFound();
+            }
+            var category = await context.Categories.FirstOrDefaultAsync(c => c.CategoryID == id);
 
-			if (category == null)
-			{
-				return NotFound();
-			}
+            if (category == null)
+            {
+                return NotFound();
+            }
 
-			return View(category);
-		}
+            return View(category);
+        }
 
-		[HttpPost, ActionName("CategoryDelete")] 
-		public async Task<IActionResult> CategoryDeleteConfirmed(int id)
-		{
-			bool answer = c.CategoryDelete(id);
-			if (answer == true)
-			{
-				TempData["Message"] = "Silindi";
-				return RedirectToAction("CategoryIndex");
-			}
-			else
-			{
-				TempData["Message"] = "HATA";
-				return RedirectToAction(nameof(CategoryDelete));
-			}
-		}
+        [HttpPost, ActionName("CategoryDelete")]
+        public async Task<IActionResult> CategoryDeleteConfirmed(int id)
+        {
+            bool answer = c.CategoryDelete(id);
+            if (answer == true)
+            {
+                TempData["Message"] = "Silindi";
+                return RedirectToAction("CategoryIndex");
+            }
+            else
+            {
+                TempData["Message"] = "HATA";
+                return RedirectToAction(nameof(CategoryDelete));
+            }
+        }
 
-		public async Task<IActionResult> SupplierIndex()
-		{
-			List<Supplier> suppliers = await s.SupplierSelect();
-			return View(suppliers);
-		}
+        public async Task<IActionResult> SupplierIndex()
+        {
+            List<Supplier> suppliers = await s.SupplierSelect();
+            return View(suppliers);
+        }
 
-		[HttpGet]
-		public IActionResult SupplierCreate()
-		{
-			return View();
-		}
+        [HttpGet]
+        public IActionResult SupplierCreate()
+        {
+            return View();
+        }
 
-		[HttpPost]
-		public IActionResult SupplierCreate(Supplier supplier)
-		{
+        [HttpPost]
+        public IActionResult SupplierCreate(Supplier supplier)
+        {
 
 
-			bool answer = s.SupplierInsert(supplier);
-			if (answer == true)
-			{
-				TempData["Message"] = supplier.BrandName + " Markası Eklendi";
-			}
-			else
-			{
-				TempData["Message"] = "HATA " + supplier.BrandName + " Markası eklenemedi.";
-			}
-			return RedirectToAction(nameof(SupplierCreate)); 
-		}
+            bool answer = s.SupplierInsert(supplier);
+            if (answer == true)
+            {
+                TempData["Message"] = supplier.BrandName + " Markası Eklendi";
+            }
+            else
+            {
+                TempData["Message"] = "HATA " + supplier.BrandName + " Markası eklenemedi.";
+            }
+            return RedirectToAction(nameof(SupplierCreate));
+        }
 
-		[HttpGet]
-		public async Task<IActionResult> SupplierEdit(int? id)
-		{
-			if (id == null || context.Suppliers == null)
-			{
-				return NotFound();
-			}
+        [HttpGet]
+        public async Task<IActionResult> SupplierEdit(int? id)
+        {
+            if (id == null || context.Suppliers == null)
+            {
+                return NotFound();
+            }
 
-			var supplier = await s.SupplierDetails(id);
+            var supplier = await s.SupplierDetails(id);
 
-			return View(supplier);
-		}
+            return View(supplier);
+        }
 
-		[HttpPost]
-		public IActionResult SupplierEdit(Supplier supplier)
-		{
-			if (supplier.PhotoPath == null)
-			{
-				string? PhotoPath = context.Suppliers.FirstOrDefault(s => s.SupplierID == supplier.SupplierID).PhotoPath;
-				supplier.PhotoPath = PhotoPath;
-			}
+        [HttpPost]
+        public IActionResult SupplierEdit(Supplier supplier)
+        {
+            if (supplier.PhotoPath == null)
+            {
+                string? PhotoPath = context.Suppliers.FirstOrDefault(s => s.SupplierID == supplier.SupplierID).PhotoPath;
+                supplier.PhotoPath = PhotoPath;
+            }
 
-			bool answer = s.SupplierUpdate(supplier);
-			if (answer == true)
-			{
-				TempData["Message"] = "Güncellendi";
-				return RedirectToAction("SupplierIndex");
-			}
-			else
-			{
-				TempData["Message"] = "HATA";
-				return RedirectToAction(nameof(SupplierEdit));
-			}
-		}
+            bool answer = s.SupplierUpdate(supplier);
+            if (answer == true)
+            {
+                TempData["Message"] = "Güncellendi";
+                return RedirectToAction("SupplierIndex");
+            }
+            else
+            {
+                TempData["Message"] = "HATA";
+                return RedirectToAction(nameof(SupplierEdit));
+            }
+        }
 
-		[HttpGet]
-		public async Task<IActionResult> SupplierDetails(int? id)
-		{
-			var supplier = await context.Suppliers.FirstOrDefaultAsync(c => c.SupplierID == id);
-			ViewBag.brandname = supplier?.BrandName;
+        [HttpGet]
+        public async Task<IActionResult> SupplierDetails(int? id)
+        {
+            var supplier = await context.Suppliers.FirstOrDefaultAsync(c => c.SupplierID == id);
+            ViewBag.brandname = supplier?.BrandName;
 
-			return View(supplier);
-		}
+            return View(supplier);
+        }
 
-		[HttpGet]
-		public async Task<IActionResult> SupplierDelete(int? id)
-		{
-			if (id == null || context.Suppliers == null)
-			{
-				return NotFound();
-			}
+        [HttpGet]
+        public async Task<IActionResult> SupplierDelete(int? id)
+        {
+            if (id == null || context.Suppliers == null)
+            {
+                return NotFound();
+            }
 
-			var supplier = await context.Suppliers.FirstOrDefaultAsync(c => c.SupplierID == id);
+            var supplier = await context.Suppliers.FirstOrDefaultAsync(c => c.SupplierID == id);
 
-			if (supplier == null)
-			{
-				return NotFound();
-			}
+            if (supplier == null)
+            {
+                return NotFound();
+            }
 
-			return View(supplier);
-		}
+            return View(supplier);
+        }
 
-		[HttpPost, ActionName("SupplierDelete")] 
-		public async Task<IActionResult> SupplierDeleteConfirmed(int id)
-		{
-			bool answer = s.SupplierDelete(id);
-			if (answer == true)
-			{
-				TempData["Message"] = "Silindi";
-				return RedirectToAction("SupplierIndex");
-			}
-			else
-			{
-				TempData["Message"] = "HATA";
-				return RedirectToAction(nameof(SupplierDelete));
-			}
-		}
+        [HttpPost, ActionName("SupplierDelete")]
+        public async Task<IActionResult> SupplierDeleteConfirmed(int id)
+        {
+            bool answer = s.SupplierDelete(id);
+            if (answer == true)
+            {
+                TempData["Message"] = "Silindi";
+                return RedirectToAction("SupplierIndex");
+            }
+            else
+            {
+                TempData["Message"] = "HATA";
+                return RedirectToAction(nameof(SupplierDelete));
+            }
+        }
 
         public async Task<IActionResult> StatusIndex()
         {
